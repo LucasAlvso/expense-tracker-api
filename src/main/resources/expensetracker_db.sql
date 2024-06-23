b5b064c0d9eef0f0139de500547c90e29a9de86d
Lucas Alves <lucas.soares008@edu.pucrs.br>
2024-06-23 01:24:42 -0300

    Reconfigured database scripts to run on application start to make running easier

--- src/main/resources/expensetracker_db.sql
+++ src/main/resources/expensetracker_db.sql
@@ -1,11 +1,3 @@
-drop database expensetrackerdb;
-drop user expensetracker;
-create user expensetracker with password 'password';
-create database expensetrackerdb with template=template0 owner=expensetracker;
-\connect expensetrackerdb;
-alter default privileges grant all on tables to expensetracker;
-alter default privileges grant all on sequences to expensetracker;
-
 create table et_users(
 user_id integer primary key not null,
 first_name varchar(20) not null,
