4d8d033131e2aade4478a5eec7990fc61b43beb8
Lucas Alves <lucas.soares008@edu.pucrs.br>
2024-06-24 20:17:21 -0300

    Criada a suite de testes unitários

--- src/test/java/com/pairlearning/expensetracker/UnitTests.java
+++ src/test/java/com/pairlearning/expensetracker/UnitTests.java
@@ -1,9 +1,20 @@
 package com.pairlearning.expensetracker;
 
+import com.pairlearning.expensetracker.unittests.AuthFilterTest;
+import com.pairlearning.expensetracker.unittests.UserRepositoryImplTest;
+import com.pairlearning.expensetracker.unittests.UserResourceTest;
+import com.pairlearning.expensetracker.unittests.UserServiceImplTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Suite.SuiteClasses({
+		AuthFilterTest.class,
+		UserRepositoryImplTest.class,
+		UserServiceImplTest.class,
+		UserResourceTest.class
+})
 class UnitTests
 {
 
